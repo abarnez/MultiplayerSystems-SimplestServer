e5c471b24848d0eef275351688216f0c2d936970
abarnez <alexander_barnes@outlook.com>
2021-10-11 19:53:08 -0400

    added 4th lab functionality (creating account)

--- SimplestServer/Assets/NetworkedServer.cs
+++ SimplestServer/Assets/NetworkedServer.cs
@@ -14,6 +14,8 @@ public class NetworkedServer : MonoBehaviour
     int hostID;
     int socketPort = 5491;
 
+    LinkedList<PlayerAccount> playerAccounts;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +25,8 @@ public class NetworkedServer : MonoBehaviour
         unreliableChannelID = config.AddChannel(QosType.Unreliable);
         HostTopology topology = new HostTopology(config, maxConnections);
         hostID = NetworkTransport.AddHost(topology, socketPort, null);
-        
+        playerAccounts = new LinkedList<PlayerAccount>();
+
     }
 
     // Update is called once per frame
@@ -57,17 +60,114 @@ public class NetworkedServer : MonoBehaviour
         }
 
     }
-  
+
     public void SendMessageToClient(string msg, int id)
     {
         byte error = 0;
         byte[] buffer = Encoding.Unicode.GetBytes(msg);
         NetworkTransport.Send(hostID, id, reliableChannelID, buffer, msg.Length * sizeof(char), out error);
     }
-    
+
     private void ProcessRecievedMsg(string msg, int id)
     {
         Debug.Log("msg recieved = " + msg + ".  connection id = " + id);
+
+        string[] csv = msg.Split(',');
+        int signifier = int.Parse(csv[0]);
+        if (signifier == ClientToServerSignifiers.CreateAccount)
+        {
+
+            string n = csv[1];
+            string p = csv[2];
+
+            bool isUnique = false;
+
+            foreach (PlayerAccount pa in playerAccounts)
+            {
+                if (pa.name == n)
+                {
+                    isUnique = true;
+                    break;
+                }
+            }
+            if (!isUnique)
+            {
+                playerAccounts.AddLast(new PlayerAccount(n, p));
+                SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.Success, id);
+                Debug.Log("it works");
+            }
+            else
+            {
+                SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.FailureNameInUse, id);
+                Debug.Log("it works");
+            }
+        }
+        else if (signifier == ClientToServerSignifiers.Login)
+        {
+
+            string n = csv[1];
+            string p = csv[2];
+
+            bool hasBeenFound = false;
+
+            foreach (PlayerAccount pa in playerAccounts)
+            {
+                if (pa.name == n)
+                {
+                    if (pa.password == p)
+                    {
+                        SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.Success, id);
+                    }
+                    else
+                    {
+                        SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.FailureIncorrectPassword, id);
+                    }
+
+
+                    hasBeenFound = true;
+                    break;
+                }
+            }
+            if (!hasBeenFound)
+            {
+                SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.FailureNameNotFound, id);
+            }
+
+        }
+
+
     }
 
+}
+
+public class PlayerAccount
+{
+    public string name, password;
+    public PlayerAccount(string Name, string Password)
+    {
+        name = Name;
+        password = Password;
+    }
+}
+
+public static class ClientToServerSignifiers
+{
+    public const int Login = 1;
+    public const int CreateAccount = 2;
+}
+
+public static class ServerToClientSignifiers
+{
+    public const int LoginResponse = 1;
+    //  public const int LoginFail = 2;
+    //public const int CreateAccountSuccess = 3;
+    //public const int CreateAccountFailure = 4;
+}
+
+public static class LoginResponses
+{
+    public const int Success = 1;
+    public const int FailureNameInUse = 2;
+    public const int FailureNameNotFound = 3;
+    public const int FailureIncorrectPassword = 4;
 }
